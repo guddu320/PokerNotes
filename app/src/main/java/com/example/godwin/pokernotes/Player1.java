6c1bf58d6807e1c7027ff8e27635ada6d4d41388
guddu320 <godwinsamuel320@gmail.com>
2017-12-21 11:11:16 +1100

    Storage working, ViewButton functions to be transferred to ViewFragment

--- app/src/main/java/com/example/godwin/pokernotes/Player1.java
+++ app/src/main/java/com/example/godwin/pokernotes/Player1.java
@@ -17,80 +17,82 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Player1 extends Fragment {
-        View view;
-        TextView day, date, time;
-        Button save, viewDetails;
-        EditText entry;
-        Storage myDb;
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState)
-        {
-            // Inflate the layout for this fragment
-            view = inflater.inflate(R.layout.activity_player1, container, false);
+    View view;
+    TextView day, date, time;
+    Button save, viewDetails;
+    EditText entry;
+    Storage myDb;
 
-            day = (TextView)view.findViewById(R.id.textView_Day);
-            date = (TextView)view.findViewById(R.id.textView_date);
-            time = (TextView)view.findViewById(R.id.textView_time);
-            entry = (EditText)view.findViewById(R.id.editText_entry);
 
-            Calendar calendar = Calendar.getInstance();
-            int x = calendar.get(Calendar.DAY_OF_WEEK);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.activity_player1, container, false);
+        myDb = new Storage(getActivity());
 
-            switch (x) {
-                case Calendar.SUNDAY:
-                    day.setText("Sunday");
-                    break;
+        day = (TextView) view.findViewById(R.id.textView_Day);
+        date = (TextView) view.findViewById(R.id.textView_date);
+        time = (TextView) view.findViewById(R.id.textView_time);
+        entry = (EditText) view.findViewById(R.id.editText_entry);
 
-                case Calendar.MONDAY:
-                    day.setText("Monday");
-                    break;
+        Calendar calendar = Calendar.getInstance();
+        int x = calendar.get(Calendar.DAY_OF_WEEK);
 
-                case Calendar.TUESDAY:
-                    day.setText("Tuesday");
-                    break;
+        switch (x) {
+            case Calendar.SUNDAY:
+                day.setText("Sunday");
+                break;
 
-                case Calendar.WEDNESDAY:
-                    day.setText("Wednesday");
-                    break;
+            case Calendar.MONDAY:
+                day.setText("Monday");
+                break;
 
-                case Calendar.THURSDAY:
-                    day.setText("Thursday");
-                    break;
+            case Calendar.TUESDAY:
+                day.setText("Tuesday");
+                break;
 
-                case Calendar.FRIDAY:
-                    day.setText("Friday");
-                    break;
+            case Calendar.WEDNESDAY:
+                day.setText("Wednesday");
+                break;
 
-                case Calendar.SATURDAY:
-                    day.setText("Saturday");
-                    break;
-            }
+            case Calendar.THURSDAY:
+                day.setText("Thursday");
+                break;
 
-            SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-            String formattedDate = df.format(calendar.getTime());
-            date.setText(formattedDate);
-            String formattedTime = Calendar.HOUR_OF_DAY + ": " + calendar.get(Calendar.MINUTE);
-            if (calendar.get(Calendar.AM_PM) == Calendar.PM) {
-                formattedTime = formattedTime+" PM";
-            }
-            else{
-                formattedTime = formattedTime+" AM";
-            }
-            time.setText(formattedTime);
+            case Calendar.FRIDAY:
+                day.setText("Friday");
+                break;
 
-            save = (Button)view.findViewById(R.id.button_save1);
-            viewDetails = (Button)view.findViewById(R.id.button_view1);
-            AddData();
-            viewAll();
-
-            return view;
+            case Calendar.SATURDAY:
+                day.setText("Saturday");
+                break;
         }
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(calendar.getTime());
+        date.setText(formattedDate);
+        String formattedTime = Calendar.HOUR_OF_DAY + ": " + calendar.get(Calendar.MINUTE);
+        if (calendar.get(Calendar.AM_PM) == Calendar.PM) {
+            formattedTime = formattedTime + " PM";
+        } else {
+            formattedTime = formattedTime + " AM";
+        }
+        time.setText(formattedTime);
+
+        save = (Button) view.findViewById(R.id.button_save1);
+        viewDetails = (Button) view.findViewById(R.id.button_view1);
+        AddData();
+        viewAll();
+
+        return view;
+    }
 
     public void AddData() {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 boolean isInserted = myDb.insertData("Player 1", day.getText().toString(), date.getText().toString(), time.getText().toString(), entry.getText().toString());
                 if (isInserted == true)
                     Toast.makeText(getActivity().getApplicationContext(), "Data Inserted", Toast.LENGTH_LONG).show();
@@ -116,14 +118,15 @@ public class Player1 extends Fragment {
                     buffer.append("Player :" + res.getString(1) + "\n");
                     buffer.append("Day :" + res.getString(2) + "\n");
                     buffer.append("Date :" + res.getString(3) + "\n");
-                    buffer.append("Time :" + res.getInt(4) +"\n");
-                    buffer.append("Notes :" + res.getInt(5) +"\n\n");
+                    buffer.append("Time :" + res.getInt(4) + "\n");
+                    buffer.append("Notes :" + res.getInt(5) + "\n\n");
                 }
 
                 showMessage("Data", buffer.toString());
             }
         });
     }
+
     public void showMessage(String title, String Message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setCancelable(true);
@@ -133,5 +136,5 @@ public class Player1 extends Fragment {
     }
 
 
-    }
+}
 
